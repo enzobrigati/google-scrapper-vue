c5b75d602fb2e070dfec23d9fc7e771f92a48561
Brigat0 <contact.mrfour@gmail.com>
2021-11-17 10:15:20 +0100

    replace API

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,48 @@
   <div id="app">
     <nav class="nav navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <router-link class="navbar-brand brand" :to="{name: 'home'}">
+        <router-link class="navbar-brand brand" :to="{ name: 'home' }">
           <span class="brand_start">G</span>
           <span class="brand_end">scrapper</span>
         </router-link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <router-link class="nav-link" :to="{name: 'home'}">Scrapper</router-link>
+              <router-link class="nav-link" :to="{ name: 'home' }"
+                >Scrapper</router-link
+              >
             </li>
           </ul>
         </div>
       </div>
     </nav>
-    <router-view/>
+    <router-view />
     <footer class="text-center mt-4">
-      <img src="@/assets/image/logo-full.svg" height="80px"/>
-      <p>© Copyright Google-Scrapper - Créé par <a href="https://deob.fr" target="_blank">Enzo Brigati</a> - Tous droits réservés - <router-link class="text-primary" :to="{name: 'changelog'}">Changelog</router-link></p>
+      <img src="@/assets/image/logo-full.svg" height="80px" />
+      <p>
+        © Copyright Google-Scrapper -
+        <a href="https://deob.fr" target="_blank">DEOB</a> - Tous droits
+        réservés -
+        <router-link class="text-primary" :to="{ name: 'changelog' }"
+          >Changelog</router-link
+        >
+      </p>
     </footer>
   </div>
 </template>
 
 <style>
-
 .router-link-exact-active {
   color: white !important;
 }
--- src/components/Scrapper.vue
+++ src/components/Scrapper.vue
@@ -2,43 +2,61 @@
   <div class="scrapper">
     <div class="container mt-4">
       <h1>Effectuer une recherche</h1>
-      <hr>
+      <hr />
       <div v-if="loading">
         <div class="alert alert-info">
-          <Loader v-if="loading"/>
+          <Loader v-if="loading" />
           Veuillez patienter pendant que nous générons votre fichier
         </div>
       </div>
       <div class="col-12 mt-2 mb-2" v-if="organicResults">
-        <download-csv class="btn btn-secondary" :data="organicResults" name="report.csv">
+        <download-csv
+          class="btn btn-secondary"
+          :data="organicResults"
+          :name="
+            this.notFormattedQuery
+              ? this.notFormattedQuery + '.csv'
+              : 'report.csv'
+          "
+        >
           Télécharger le rapport
         </download-csv>
       </div>
-      <FormulateForm v-model="formData" @submit="handleSubmit" name="scrapperForm" class="row">
+      <FormulateForm
+        v-model="formData"
+        @submit="handleSubmit"
+        name="scrapperForm"
+        class="row"
+      >
         <div class="col-12 col-md-6">
-          <FormulateInput label="Requête à effectuer"
-                          v-model="query"
-                          name="query"
-                          type="text"
-                          validation="required"
-                          validation-name="La requête"
-                          placeholder="Votre requête"
+          <FormulateInput
+            label="Requête à effectuer"
+            v-model="query"
+            name="query"
+            type="text"
+            validation="required"
+            validation-name="La requête"
+            placeholder="Votre requête"
           />
         </div>
         <div class="col-12 col-md-6">
-          <FormulateInput label="Nombre de résultat à afficher"
-                          v-model="resultNumber"
-                          name="resultNumber"
-                          type="number"
-                          validation="required"
-                          validation-name="Le nombre de résultat"
-                          placeholder="Nombre de résultat"
+          <FormulateInput
+            label="Nombre de résultat à afficher"
+            v-model="resultNumber"
+            name="resultNumber"
+            type="number"
+            validation="required"
+            validation-name="Le nombre de résultat"
+            placeholder="Nombre de résultat"
           />
         </div>
         <div class="col-12 col-md-2">
-          <FormulateInput type="submit" label="Scrapper !"
-                          :input-class="['btn btn-primary']"
-                          :disabled="loading"/>
+          <FormulateInput
+            type="submit"
+            label="Scrapper !"
+            :input-class="['btn btn-primary']"
+            :disabled="loading"
+          />
         </div>
       </FormulateForm>
     </div>
@@ -50,37 +68,49 @@ import axios from "axios";
 import Loader from "@/Ui/Loader";
 
 export default {
-  name: 'Scrapper',
+  name: "Scrapper",
   components: {
-    Loader
+    Loader,
   },
-  data: function () {
+  data: function() {
     return {
       query: null,
       resultNumber: 10,
       loading: false,
       formData: null,
-      organicResults: null
-    }
+      organicResults: null,
+      notFormattedQuery: null,
+    };
   },
   methods: {
-    handleSubmit: async function () {
-      this.loading = true
-      const parsedQuery = new URLSearchParams({
-        query: this.query
-      })
-      let url = 'http://api.serpstack.com/search?access_key=' + process.env.VUE_APP_SERPSTACK_APIKEY + '&' + parsedQuery + '&engine=google&hl=fr&gl=fr&num=' + this.resultNumber
-      await axios.get(url)
-          .then(res => {
-            this.organicResults = res.data['organic_results']
-            console.log(res)
-          }).catch(e => console.warn(e))
-      this.loading = false
-    }
+    handleSubmit: async function() {
+      this.loading = true;
+      this.notFormattedQuery = this.query;
+      let parsedQuery = new URLSearchParams({
+        query: this.query,
+      });
+      let url =
+        "https://rt.serpmaster.com/?access_token=" +
+        process.env.VUE_APP_SERPSTACK_APIKEY +
+        "&" +
+        parsedQuery +
+        "&scrapper=google_search&geo=France&parse=true&domain=fr&locale=fr-fr&device=desktop&num=" +
+        this.resultNumber;
+      await axios
+        .get(url)
+        .then((res) => {
+          this.organicResults = res.data.results.content.results.organic;
+          this.query = null;
+          this.notFormattedQuery = null;
+          console.log(res);
+        })
+        .catch((e) => console.warn(e));
+      this.loading = false;
+    },
+    addQueryComponent: function() {},
   },
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
